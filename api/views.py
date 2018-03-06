8bf2ff2a08bc342538fa26022119c555bae650e2
Keizashi <Suhoffpi-88@yandex.ru>
2018-03-06 15:08:07 +0400

    Jobs done. Working django-api-app

--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
 import json
 from django.http import HttpResponse, JsonResponse
+import requests
 
-# def Index(request, vkid):
-#     json_object = {'vkid': '1'}
-#     return JsonResponse(json_object)
 
-def your_view(request, vkid):
-    json_object = {'key': vkid}
-    return JsonResponse(json_object)
+def Index(request, vkid):
+    jopa = requests.get('https://api.vk.com/method/users.get?v=5.73&user_ids=' + str(vkid) + '&fields=photo_50')
+
+    data = {
+        "vk_data": {
+            "id": vkid,
+            "first_name": jopa.json()['response'][0]['first_name'],
+            "last_name":  jopa.json()['response'][0]['last_name'],
+            "photo_url":  jopa.json()['response'][0]['photo_50']
+        }
+    }
+    return JsonResponse(data, safe=False)
