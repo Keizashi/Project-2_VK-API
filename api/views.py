46c47fd2fbf490c22a850e8cf9df4ff2df200c21
Keizashi <Suhoffpi-88@yandex.ru>
2018-03-07 18:44:53 +0400

    Jobs done. Working django-api-app. Edit: changed dict name

--- api/views.py
+++ api/views.py
@@ -4,14 +4,14 @@ import requests
 
 
 def Index(request, vkid):
-    jopa = requests.get('https://api.vk.com/method/users.get?v=5.73&user_ids=' + str(vkid) + '&fields=photo_50')
+    dict = requests.get('https://api.vk.com/method/users.get?v=5.73&user_ids=' + str(vkid) + '&fields=photo_50')
 
     data = {
         "vk_data": {
             "id": vkid,
-            "first_name": jopa.json()['response'][0]['first_name'],
-            "last_name":  jopa.json()['response'][0]['last_name'],
-            "photo_url":  jopa.json()['response'][0]['photo_50']
+            "first_name": dict.json()['response'][0]['first_name'],
+            "last_name":  dict.json()['response'][0]['last_name'],
+            "photo_url":  dict.json()['response'][0]['photo_50']
         }
     }
     return JsonResponse(data, safe=False)
